79bea919d428f4e04873e059b25b18b3090c7a82
anwebdevelopers <anwebdev78@gmail.com>
2017-10-20 19:46:33 +0300

    20.10.2017

--- html/js/common.js
+++ html/js/common.js
@@ -18,7 +18,7 @@ $(function() {
         $headerButtonMenu = $('.header__button-menu');
     $headerButtonMenu.click(function() {
         $(this).toggleClass('active');
-        $headerMenu.slideToggle(500);
+        $headerMenu.slideToggle(300);
     });
 
     /*******************************************************/
@@ -26,11 +26,11 @@ $(function() {
     /*******************************************************/
     $headerMenu.find('.has-submenu a').click(function(e) {
         w = $window.width();
-        if (w <= 768) {
+        if ( w <= 768 ) {
             var $this = $(this),
                 $ul = $this.next('ul'),
                 $li = $this.closest('li');
-            if (!$li.hasClass('active')) {
+            if ( !$li.hasClass('active') ) {
                 e.preventDefault();
                 $ul.slideDown(300);
                 $li.addClass('active').siblings().removeClass('active').find('ul').slideUp(300);
@@ -44,13 +44,56 @@ $(function() {
     });
 
     //*****************************************************//
-    //Product Tabs
+    //DIRECTORY SWITCH
     //*****************************************************//
 
     $('.directory').each(function() {
-        $(this).find('.directory__item').not(':first-child').hide();
+        $(this).find('.directory__item').not(':first').hide();
         $(this).find('.directory__switch').on('click', 'button:not(.active)', function() {
             $(this).addClass('active').siblings().removeClass('active').closest('.directory').find('.directory__item').slideUp(200).eq($(this).index()).slideDown(200);
         }).find('button').first().addClass('active');
     });
+
+    /*******************************************************/
+    //RECENT SLIDER
+    /*******************************************************/
+    $('.recent__box').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: '',
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 300,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            361: {
+                items: 2
+            },
+            769: {
+                items: 3
+            }
+        },
+        onInitialized: function() {
+            $('.recent__box .owl-next').before($('.recent__box .owl-dots'));
+        }
+    });
+
+    /*******************************************************/
+    //ABOUT READ MORE
+    /*******************************************************/
+    $('.about__content').each(function() {
+        $(this).children().not(':first').hide();
+        $(this).after('<button class="about__button">подробнее</button>');
+        $('.about__button').on('click', function() {
+            if ( $(this).hasClass('active') ) {
+                $(this).removeClass('active').text('подробнее').prev('.about__content').children().not(':first').slideUp(300);
+            } else {
+                $(this).addClass('active').text('свернуть').prev('.about__content').children().not(':first').slideDown(300);
+            }
+        });
+    });
+
 });
